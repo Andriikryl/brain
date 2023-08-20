eba69489ef894ba8b4e873779cd16fce25622de7
Andriikryl <andreyk557@gmail.com>
2023-08-20 17:44:08 +0300

    add cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
 
 function App() {
 
   return (
-    <h1>asd</h1>
+    <Canvas>
+      <ambientLight/>
+      <pointLight position={[10, 10, 10]}/>
+      <mesh>
+        <boxGeometry args={[1,1,1]}></boxGeometry>
+        <meshStandardMaterial color="tomato"/>
+      </mesh>
+      <OrbitControls/>
+    </Canvas>
   )
 }
 
